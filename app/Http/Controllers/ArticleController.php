ccf8853fad74e380bde8e58f13f542cd633c1503
m5151128 <nagayama@sencorp.co.jp>
2018-03-11 22:44:20 +0900

    edit controller article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        $articles = Article::all();
+        return view('article.index', ['articles' => $articles]);
+    }
 }
